5737dfeda1487cf06f7f5c4b3c00e553f5218e90
raymondcmd <raymondcmd@gmail.com>
2019-01-03 13:53:58 -0500

    CV Kalao Versioned

---
+++ app/src/main/java/com/hamptontechs/cvkalao/StickerPack.java
@@ -0,0 +1,13 @@
+package com.hamptontechs.cvkalao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StickerPack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_pack);
+    }
+}
